7ab0d9b59cb53311015035c7572a9ef099c3e105
Denis Gubanov <v12aml@gmail.com>
2017-02-05 20:03:15 +0300

    pjsip

--- ippbx.py
+++ ippbx.py
@@ -89,26 +89,33 @@ def asterisk_sip_user_config(phonenum, username):
 def asterisk_pjsip_user_config(phonenum, username):
     """generate sip user config for Asterisk"""
     log_debug("Generating PJSIP config for {} {}".format(phonenum, username))
-    user_config = ""
-    user_config += "[" + phonenum + "]" + "\n"
-    user_config += "type=friend" + "\n"
-    user_config += "host=dynamic" + "\n"
-    user_config += "username=" + phonenum + "\n"
-    user_config += "secret=" + genuserpass(phonenum) + "\n"
-    user_config += "fullname=" + phonenum + "\n"
-    user_config += "callerid=" + username + "\n"
+    user_pass = genuserpass(phonenum)
+    user_config = ";{} <{}> {}".format(phonenum, username, user_pass)
+    user_config += "\n"
+    user_config += "[{}]".format(phonenum) + "\n"
+    user_config += "type=auth" + "\n"
+    user_config += "auth_type=userpass" + "\n"
+    user_config += "username={}".format(phonenum) + "\n"
+    user_config += "password={}".format(user_pass) + "\n"
+    user_config += "" + "\n"
+    user_config += "[{}]".format(phonenum) + "\n"
+    user_config += "type=aor" + "\n"
+    user_config += "max_contacts=1" + "\n"
+    user_config += "remove_existing=yes" + "\n"
+    user_config += "qualify_frequency=5" + "\n"
+    user_config += "" + "\n"
+    user_config += "[{}]".format(phonenum) + "\n"
+    user_config += "type=endpoint" + "\n"
     user_config += "context=" + asterisk_user_context + "\n"
-    user_config += "transport=udp" + "\n"
     user_config += "disallow=all" + "\n"
     user_config += "allow=" + asterisk_codecs_allow + "\n"
-    user_config += "canreinvite=no" + "\n"
-    user_config += "nat=yes" + "\n"
-    user_config += "qualify=yes" + "\n"
-    user_config += "hassip=yes" + "\n"
-    user_config += "hasiax=no" + "\n"
-    user_config += "hash323=no" + "\n"
-    user_config += "hasmanager=no" + "\n"
-    user_config += "\n"
+    user_config += "aors={}".format(phonenum) + "\n"
+    user_config += "auth={}".format(phonenum) + "\n"
+    user_config += "rtp_symmetric=yes" + "\n"
+    user_config += "rtp_ipv6=yes" + "\n"
+    user_config += "rewrite_contact=yes" + "\n"
+    user_config += "send_rpid=yes" + "\n"
+    user_config += "" + "\n"
     cfg_file_name = asterisk_pjsip_conf_dir + "user" + username + ".conf"
     with open(cfg_file_name, 'w') as f:
         f.write(user_config)
@@ -230,24 +237,28 @@ for entry in connection.entries:
     log_debug(entry.displayName)
     log_debug(entry.ipPhone)
     log_debug(entry.employeeID)
+    # Generating asterisk config (SIP or/and PJSIP)
     if asterisk_pjsip_enables == "True" or "true":
         asterisk_pjsip_user_config(entry.ipPhone, entry.displayName)
     if asterisk_sip_enables == "True" or "true":
         asterisk_sip_user_config(entry.ipPhone, entry.displayName)
+    # Generating Yelink phone config
     try:
-        phoneid = entry.employeeID
-        phonetype = phoneid[:1]
-        phonehwmac = str(phoneid[2:]).lower()
+        phone_id = entry.employeeID
+        phone_type = phone_id[:1]
+        phone_hwmac = str(phone_id[2:]).lower()
     except:
-        phoneid = None
-        phonetype = None
-        phonehwmac = None
-    if phonetype:
+        phone_id = None
+        phone_type = None
+        phone_hwmac = None
+        log_debug("Wrong format of employeeID LDAP field")
+    if phone_type:
         yealink_phone_config(
-            phonetype,
-            phonehwmac,
+            phone_type,
+            phone_hwmac,
             entry.ipPhone,
             entry.displayName)
+    log_debug("We are done with {}".format(entry.displayName))
 
 
 # EOF
